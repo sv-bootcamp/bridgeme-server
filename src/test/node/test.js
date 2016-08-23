13cc898d1b9ce65486c223e8a95ae56c575552ff
SarahhhC <SarahhhC@users.noreply.github.com>
2016-08-23 17:08:31 +0900

    Update test.js

--- src/test/node/test.js
+++ src/test/node/test.js
@@ -1,10 +1,12 @@
 import test from 'tape';
 
+//sample test for 'pass'
 test('A passing test', (t) => {
   t.pass('This test will pass.');
   t.end();
 });
 
+//sample test for 'equal'
 test('Assertions with tape.', (t) => {
   const expected = 'something to test';
   const actual = 'something to test';
