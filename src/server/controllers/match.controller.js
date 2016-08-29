a7a07864bb7405a5cc6b0853da6364108fefc86d
KaylaKwon <papermoon703@gmail.com>
2016-08-29 10:07:32 +0900

    set environment

--- src/server/controllers/match.controller.js
+++ src/server/controllers/match.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const matchSchema = require('mongoose').model('match');
+import mailer from 'nodemailer';
 
 let matchData = new matchSchema();
+
 let sampleResult = {
   successCode: 1,
   matchData: matchData,
@@ -89,4 +91,45 @@ export function rejectRequest(req, res, next) {
   // update mentor and mentee's request db
 
   res.json(sampleResult);
+}
+
+export function sendRequestMail(req, res, next) {
+  matchData.mentor = 'mentor_reject@gmail.com';
+  matchData.mentee = 'mentee_reject@gmail.com';
+
+  let transport = mailer.createTransport("SMTP", {
+    service: 'Gmail',
+    auth: {
+      user: 'yoda.mentor.lab@gmail.com',
+      pass: 'svbootcamp@!',
+    },
+  });
+
+  let mailOptions = {
+    from: matchData.mentor,
+    to: matchData.mentor,
+    subject: 'Would you be my Yoda?',
+    text: 'Hi!',
+    html: '<h1>HTML 보내기 테스트</h1><p><img src="http://www.nodemailer.com/img/logo.png"/></p>',
+  };
+
+  transport.sendMail(mailOptions, function(err, response){
+    let failureResult = {
+      successCode: 0,
+      result: err,
+    };
+
+    let successResult = {
+      successCode: 1,
+      mailOptions: mailOptions,
+    };
+
+    if (err){
+      res.json(failureResult);
+    } else {
+      res.json(successResult);
+    }
+
+    transport.close();
+  });
 }
--- src/server/routes/match.route.js
+++ src/server/routes/match.route.js
@@ -9,5 +9,6 @@ router.get('/request', match.requestMentoring);
 router.get('/cancel', match.cancelRequest);
 router.get('/accept', match.acceptRequest);
 router.get('/reject', match.rejectRequest);
+router.get('/smtp', match.sendRequestMail);
 
 export default router;
