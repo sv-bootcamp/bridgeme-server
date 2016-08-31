7fc355e1df17dd33e3f06795d2aff2402ad9b91c
SarahhhC <joytutu29@naver.com>
2016-08-31 18:03:39 +0900

    complete feature

--- src/test/controllers/users.controller.test.js
+++ src/test/controllers/users.controller.test.js
@@ -24,7 +24,7 @@ describe('Test for users.controller', function() {
         // TODO: Add a test to access getMentorList method.
         // TODO: Add a test to check for non-existent user session.
         // TODO: Add a test to check if all users are returned except the user in the session.
-        //createdUser.remove();
+        createdUser.remove();
         done();
       });
     });
