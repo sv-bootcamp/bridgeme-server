5ccb458da76926fb20b57f31b142a4ca51def550
LIM DONG JOO <dongdongalcohol@gmail.com>
2016-09-02 04:42:11 +0900

    0902

--- src/server/models/users.model.js
+++ src/server/models/users.model.js
@@ -1,35 +1,29 @@
-'use strict';
-
 import mongoose from 'mongoose';
-
 const Schema = mongoose.Schema;
+
 let userSchema = new Schema({
-    userId: String,
-    email:
-    { type: String,
-        unique: true,
-        required: true
-    },
-    platform_id: String,
-    platform_type:
-    { type: Number,
-        unique: true,
-        required: true
-    },
-    name: String,
-    age: Number,
+    userId: { type: String, unique: true },
+    email: { type: String, unique: true },
+    firstName: String,
+    lastName: String,
+    summary: String,
+    platform_type: Number,
+    platform_id: { type: String, unique: true },
     field: String,
-    region: String,
-    skills: String,
+    location: String,
+    positions: Schema.Types.ObjectId,
+    educations: Schema.Types.ObjectId,
+    skills: Schema.Types.ObjectId,
     regDate: {
         type: Date,
         default: Date.now(),
-    },
+      },
+    profilePictureURL: String,
 });
 
 userSchema.set('toJSON', {
     getters: true,  // bring all elements include virtuals
     virtuals: true,  // To have all virtuals show up in your console.log output
-});
+  });
 
 export default mongoose.model('user', userSchema);
