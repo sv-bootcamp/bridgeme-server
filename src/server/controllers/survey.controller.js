489035c6cbe0f3e91b253fd52080f59e5b113293
KaylaKwon <papermoon703@gmail.com>
2016-09-05 14:28:33 +0900

    Todo list

--- src/server/controllers/survey.controller.js
+++ src/server/controllers/survey.controller.js
@@ -3,12 +3,16 @@ import mongoose from 'mongoose';
 const Survey = mongoose.model('survey');
 const Answer = mongoose.model('answer');
 
-// // Send survey.
-// export function getSurvey() {
-//
-// }
-//
-// // Get answer.
-// export function getAnswer(survey_id, is_multiple, options_num, options) {
-//
-// }
+// Send survey
+export function getSurvey(res, survey_id) {
+  // Todo:
+  // 1. find survey with survey_id
+  // 2. res.json( survey content )
+}
+
+// Get answer
+export function getAnswer(res, req, next) {
+  // Todo:
+  // 1.
+  // 2.
+}
