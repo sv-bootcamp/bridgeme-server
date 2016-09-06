b1d36a6f54f4912401038059a2934e45629ce8bf
Yangwoo Lee <lyw0149@gmail.com>
2016-09-06 14:16:54 +0900

    Sign in with just access token

--- src/server/controllers/users.controller.js
+++ src/server/controllers/users.controller.js
@@ -127,7 +127,8 @@ function storeSession(req, res, user) {
 }
 
 function validateAccessTokenFacebook(accessToken, callback) {
-  request.get('https://graph.facebook.com/me?access_token=' + accessToken, function (error, response, body) {
+  request.get('https://graph.facebook.com/me?fields=name,email,locale,timezone,verified&access_token=' + accessToken, function (error, response, body) {
+    console.log(body)
     if (!error && response.statusCode == 200) {
       callback(JSON.parse(body));
     } else {
