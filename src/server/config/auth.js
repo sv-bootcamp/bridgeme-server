e77e600594ce375859537c26f4c03516d6e28ea5
Yangwoo Lee <lyw0149@gmail.com>
2016-09-07 14:51:51 +0900

    Adjust comment.

--- src/server/config/auth.js
+++ src/server/config/auth.js
@@ -1,18 +1,17 @@
 import request from 'request';
 
 export function crawlByAccessTokenFacebook(accessToken, callback) {
-  //Crawl user data from facebook by access token.
+  // Crawl user data from facebook by access token.
   request.get('https://graph.facebook.com/me?fields=name,email,locale,timezone,verified&access_token='
     + accessToken, function (error, response, userBody) {
     if (!error && response.statusCode == 200) {
       let result = JSON.parse(userBody);
-      //Crawl user profile_picture from facebook by access token.
+      // Crawl user profile_picture from facebook by access token.
       request.get('https://graph.facebook.com/'
         + result.id
         + '/picture?type=large&redirect=0', function (error, response, pictureBody) {
         if (!error && response.statusCode == 200) {
           result.profile_picture = JSON.parse(pictureBody).data.url;
-          console.log(result);
           callback(result);
         } else {
           callback();
