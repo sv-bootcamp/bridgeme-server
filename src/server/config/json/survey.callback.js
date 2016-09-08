bdd46e221435da07f0317040cca3afeca71f9b7f
KaylaKwon <papermoon703@gmail.com>
2016-09-08 09:54:44 +0900

    update

---
+++ src/server/config/json/survey.callback.js
@@ -0,0 +1,4 @@
+export default {
+  ERR_GET_SURVEY: 'Survey Not Found - No Matching survey_id',
+  ERR_SAVE_ANSWER: 'User Not Found - Cannot save survey answer. No Matching user.',
+};
--- src/server/controllers/survey.controller.js
+++ src/server/controllers/survey.controller.js
@@ -1,82 +1,40 @@
 import mongoose from 'mongoose';
+import callbackMsg from '../config/json/survey.callback';
 
 const Answer = mongoose.model('answer');
 const Survey = mongoose.model('survey');
 const User = mongoose.model('user');
 
-let successResult = {
-  success_code: 1,
-};
-
-let failureResult = {
-  success_code: 0,
-  err_point: null,
-  err_msg: null,
-};
-
-let surveyQuestion = {
-  survey_id: 'A-001',
-  questions: [
-    {
-      question_index: 1,
-      question: '왜 수락해 주셨나요?',
-      allow_multiple_answer: true,
-      answers: [
-        {
-          answer_index: 0,
-          is_free_form: false,
-          content: '멘티가 마음에 들었다',
-          next_question_index: 0,
-        },
-        {
-          answer_index: 1,
-          is_free_form: false,
-          content: '봉사가 하고 싶어서',
-          next_question_index: 1,
-        },
-        {
-          answer_index: 2,
-          is_free_form: true,
-          next_question_index: null,
-        },
-      ],
-    },
-  ],
-};
-
 // Get request
 export function getRequest(req, res, next) {
   let t = Math.random() * 100; // random number between 0 ~ 101
   if (t > -1) {
-    let survey_id;
-    if ( req.params.type == 'mentee') {
-      survey_id = 'A001-1';
-      getSurvey(res, survey_id);
+    let surveyId;
+    if (req.params.type == 'mentee') {
+      surveyId = 'A001-1';
+      getSurvey(res, surveyId);
     } else {
-      survey_id = 'B001-1';
-      getSurvey(res, survey_id);
+      surveyId = 'B001-1';
+      getSurvey(res, surveyId);
     }
   } else {
-    successResult.success_code = 2;
-    successResult.msg = 'Not in the sample boundary';
-    res.json(successResult);
+    res.status(204).json();  // 204: No content
   }
 }
 
 // Send survey
-export function getSurvey(res, survey_id) {
-  Survey.find({ survey_id: survey_id }, (err, surveyItem) => {
-      if (err) {
-        failureResult.err_point = 'Survey - getSurvey';
-        failureResult.err_msg = err;
-        res.json(failureResult);
-      } else {
-        successResult.survey_id = survey_id;
-        successResult.questions = surveyItem.questions;
-        res.json(surveyItem);
-      }
+export function getSurvey(res, surveyId) {
+  Survey.find({ survey_id: surveyId }, (err, surveyItem) => {
+    if (err) {
+      res.status(400).json({ err_point: callbackMsg.ERR_GET_SURVEY, err_msg: err });
+    } else {
+      let result = {
+        survey_id: surveyId,
+        questions: surveyItem.questions,
+      };
+      res.status(200).json(result);
     }
-  );
+  });
 }
 
 // Get answer
@@ -86,16 +44,14 @@ export function saveAnswer(req, res, next) {
     let answer = new Answer();
     answer.survey_id = req.body.survey_id;
     answer.questions = req.body.questions;
-    User.findOne({ email: req.session.email }, (err, user)=>{
+    User.findOne({ email: req.session.email }, (err, user) => {
       if (err) {
-        failureResult.err_point = 'Survey - findUserSession with email';
-        failureResult.err_msg = err;
-        res.json(failureResult);
+        res.status(400).json({ err_point: callbackMsg.ERR_GET_SURVEY, err_msg: err });
       } else {
         answer.user_id = user._id;
       }
     });
-  
+
     answer.save((err, answerItem) => {
       if (err) {
         failureResult.err_point = 'Survey - saveAnswer';
