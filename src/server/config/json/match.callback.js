b5d0f706e2f27045934aafa35ee7587e7cadc561
Yangwoo Lee <lyw0149@gmail.com>
2016-09-12 13:13:11 +0900

    Upper case to lower case.

--- src/server/config/json/match.callback.js
+++ src/server/config/json/match.callback.js
@@ -1,8 +1,8 @@
 export default {
   ERR_CANNOT_FOUND_MENTOR: 'RequestMentoring - Cannot found mentor.',
-  ERR_FAIL_SEND_MAIL: 'SendMail - Fail to Send an email.',
+  ERR_FAIL_SEND_MAIL: 'SendMail - Fail to send an email.',
   ERR_MATCH_ALREADY_EXIST: 'RequestMentoring - Match already exist.',
   ERR_MONGOOSE: 'An error occured at mongoose.',
-  SUCCESS_SEND_MAIL: 'SendMail - Success to Send an email.',
+  SUCCESS_SEND_MAIL: 'SendMail - Success to send an email.',
   SUCCESS_RESPONSE: 'ResponseMentoring - Response success.',
 };
