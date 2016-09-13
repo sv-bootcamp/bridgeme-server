41e534f0fb7f856b7df97b20535522e288d99822
KaylaKwon <papermoon703@gmail.com>
2016-09-13 11:01:49 +0900

    Set authorize condition to survey functions

--- src/server/controllers/survey.controller.js
+++ src/server/controllers/survey.controller.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-import callbackMsg from '../config/json/survey.callback';
+import surveyCallback from '../config/json/survey.callback';
+import userCallback from '../config/json/user.callback';
 
 /*
  * Methods about Eval Framework, save or return survey/answer
@@ -11,32 +12,36 @@ const User = mongoose.model('user');
 
 // Get request
 export function getRequest(req, res, next) {
-  let n = Math.random() * 100; // random number between 0 ~ 101
-  let t = -1;
-  // TODO: traffic management
-  // In this version we send survey to all user.
-  // after launch we can manage traffic by setting t.
-  if (n > t) {
-    let surveyId;
-    if (req.params.type == 'mentee') {
-      surveyId = 'A001-1';
-      getSurvey(res, surveyId);
-    } else if (req.params.type == 'mentor') {
-      surveyId = 'B001-1';
-      getSurvey(res, surveyId);
+  if (req.session._id) {
+    let n = Math.random() * 100; // random number between 0 ~ 101
+    let t = -1;
+    // TODO: traffic management
+    // In this version we send survey to all user.
+    // after launch we can manage traffic by setting t.
+    if (n > t) {
+      let surveyId;
+      if (req.params.type == 'mentee') {
+        surveyId = 'A001-1';
+        getSurvey(res, surveyId);
+      } else if (req.params.type == 'mentor') {
+        surveyId = 'B001-1';
+        getSurvey(res, surveyId);
+      } else {
+        res.status(400).json({ err_point: surveyCallback.ERR_INVALID_PARAMS });
+      }
     } else {
-      res.status(400).json({ err_point: callbackMsg.ERR_INVALID_PARAMS });
+      res.status(204).json();  // 204: No content
     }
   } else {
-    res.status(204).json();  // 204: No content
+    res.status(401).json({ err_point: userCallback.ERR_FAIL_AUTH });
   }
 }
 
 // Send survey
-export function getSurvey(res, surveyId) {
-  Survey.find({ survey_id: surveyId }, (err, surveyItem) => {
+function getSurvey(res, surveyId) {
+  Survey.findOne({ survey_id: surveyId }, (err, surveyItem) => {
     if (err) {
-      res.status(400).json({ err_point: callbackMsg.ERR_GET_SURVEY, err_msg: err });
+      res.status(400).json({ err_point: surveyCallback.ERR_GET_SURVEY, err_msg: err });
     } else {
       res.status(200).json(surveyItem);
     }
@@ -52,12 +57,12 @@ export function saveAnswer(req, res, next) {
 
     User.findOne({ _id: req.session._id }, (err, user) => {
       if (err) {
-        res.status(400).json({ err_point: callbackMsg.ERR_USER_NOT_FOUND, err_msg: err });
+        res.status(400).json({ err_point: surveyCallback.ERR_USER_NOT_FOUND, err_msg: err });
       } else {
         answer.user_id = user._id;
         answer.save((err, answerItem) => {
           if (err) {
-            res.status(400).json({ err_point: callbackMsg.ERR_SAVE_ANSWER, err_msg: err });
+            res.status(400).json({ err_point: surveyCallback.ERR_SAVE_ANSWER, err_msg: err });
           } else {
             res.status(200).json({ survey_id: answerItem.survey_id });
           }
@@ -66,29 +71,33 @@ export function saveAnswer(req, res, next) {
     });
 
   } else {
-    res.status(400).json({ err_point: callbackMsg.ERR_INVALID_ACCESS });
+    res.status(401).json({ err_point: userCallback.ERR_FAIL_AUTH });
   }
 }
 
 // Save question
 export function saveSurvey(req, res, next) {
-  let survey = new Survey({
-    survey_id: req.params.surveyQuestion.survey_id,
-    questions: req.params.surveyQuestion.questions,
-  });
+  if (req.session._id) {
+    let survey = new Survey({
+      survey_id: req.params.surveyQuestion.survey_id,
+      questions: req.params.surveyQuestion.questions,
+    });
 
-  if (survey.survey_id == null || survey.questions == null) {
-    res.status(400).json({
-      err_point: callbackMsg.ERR_SAVE_QUESTION,
-      err_msg: callbackMsg.ERR_INVALID_PARAMS,
-    });
+    if (survey.survey_id == null || survey.questions == null) {
+      res.status(400).json({
+        err_point: surveyCallback.ERR_SAVE_QUESTION,
+        err_msg: surveyCallback.ERR_INVALID_PARAMS,
+      });
+    } else {
+      survey.save((err, surveyItem) => {
+        if (err) {
+          res.status(400).json({ err_point: surveyCallback.ERR_SAVE_QUESTION, err_msg: err });
+        } else {
+          res.status(200).json({ survey_id: surveyItem.survey_id });
+        }
+      });
+    }
   } else {
-    survey.save((err, surveyItem) => {
-      if (err) {
-        res.status(400).json({ err_point: callbackMsg.ERR_SAVE_QUESTION, err_msg: err });
-      } else {
-        res.status(200).json({ survey_id: surveyItem.survey_id });
-      }
-    });
+    res.status(401).json({ err_point: userCallback.ERR_FAIL_AUTH });
   }
 }
