4a021c61ae898abd2a6a8ddd192c66fae5f333ae
KaylaKwon <papermoon703@gmail.com>
2016-09-20 10:19:36 +0900

    Use Promise to survey functions

--- src/server/config/json/survey.callback.js
+++ src/server/config/json/survey.callback.js
@@ -1,5 +1,5 @@
 export default {
-  ERR_GET_SURVEY: 'Survey Not Found - No Matching survey_id',
+  ERR_SURVEY_NOT_FOUND: 'Survey Not Found - No Matching survey_id',
   ERR_INVALID_PARAMS: 'Parameter /:type/ is not correct',
   ERR_SAVE_ANSWER: 'Cannot save answer',
   ERR_SAVE_QUESTION: 'Cannot save question',
