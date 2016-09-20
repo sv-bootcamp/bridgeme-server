9b63bc2c51daa13b6b849b2133fa9fe0b465b4c7
SarahhhC <joytutu29@naver.com>
2016-09-20 16:52:02 +0900

    hide test server part

--- src/server/index.js
+++ src/server/index.js
@@ -24,58 +24,58 @@ export default (cb) => {
     app.use(compress());
   }
 
-  if (process.env.NODE_ENV === 'development') {
-    app.use(session({
-      secret: 'yodasalt46787134refgr45refd',
-      store: new MongoStore({ db: 'yoda' }),
-      resave: false,
-      saveUninitialized: false,
-      //session expire after 1Day.
-      cookie: { maxAge: 1000 * 3600 * 24 },
-    }));
+  //if (process.env.NODE_ENV === 'development') {
+  app.use(session({
+    secret: 'yodasalt46787134refgr45refd',
+    store: new MongoStore({ db: 'yoda' }),
+    resave: false,
+    saveUninitialized: false,
+    //session expire after 1Day.
+    cookie: { maxAge: 1000 * 3600 * 24 },
+  }));
 
-    app.use(bodyParser.urlencoded({
-      extended: true,
-    }));
-    app.use(bodyParser.json());
-    app.use(methodOverride());
+  app.use(bodyParser.urlencoded({
+    extended: true,
+  }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
 
-    app.use('/users', users);
-    app.use('/survey', survey);
-    app.use('/match', match);
+  app.use('/users', users);
+  app.use('/survey', survey);
+  app.use('/match', match);
 
-    server = app.listen(8000, cb ? cb : () => {
-      /* eslint-disable no-console */
-      console.log(`Listening on port 8000`);
-      /* eslint-enable */
-    });
+  server = app.listen(8000, cb ? cb : () => {
+    /* eslint-disable no-console */
+    console.log(`Listening on port 8000`);
+    /* eslint-enable */
+  });
 
-  } else if (process.env.NODE_ENV === 'test') {
-    app.use(session({
-      secret: 'yodasalt46787asdasdaefgr45refd',
-      store: new MongoStore({ db: 'yoda-test' }),
-      resave: false,
-      saveUninitialized: false,
-      //session expire after 1Day.
-      cookie: { maxAge: 1000 * 3600 * 24 },
-    }));
-
-    app.use(bodyParser.urlencoded({
-      extended: true,
-    }));
-    app.use(bodyParser.json());
-    app.use(methodOverride());
-
-    app.use('/users', users);
-    app.use('/survey', survey);
-    app.use('/match', match);
-
-    server = app.listen(8080, cb ? cb : () => {
-      /* eslint-disable no-console */
-      console.log(`Listening on test port 8080`);
-      /* eslint-enable */
-    });
-  }
+  // } else if (process.env.NODE_ENV === 'test') {
+  //   app.use(session({
+  //     secret: 'yodasalt46787asdasdaefgr45refd',
+  //     store: new MongoStore({ db: 'yoda-test' }),
+  //     resave: false,
+  //     saveUninitialized: false,
+  //     //session expire after 1Day.
+  //     cookie: { maxAge: 1000 * 3600 * 24 },
+  //   }));
+  //
+  //   app.use(bodyParser.urlencoded({
+  //     extended: true,
+  //   }));
+  //   app.use(bodyParser.json());
+  //   app.use(methodOverride());
+  //
+  //   app.use('/users', users);
+  //   app.use('/survey', survey);
+  //   app.use('/match', match);
+  //
+  //   server = app.listen(8080, cb ? cb : () => {
+  //     /* eslint-disable no-console */
+  //     console.log(`Listening on test port 8080`);
+  //     /* eslint-enable */
+  //   });
+  // }
 
   return server;
 };
