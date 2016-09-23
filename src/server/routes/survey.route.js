726d99772ccf72153a78a5668c523ed7a4743f65
SarahhhC <joytutu29@naver.com>
2016-09-23 16:20:04 +0900

    modify apidoc answer

--- src/server/routes/survey.route.js
+++ src/server/routes/survey.route.js
@@ -25,14 +25,18 @@ const router = express.Router();
  *           "question": "이 멘토를 통해서 얻고자 하는 것은?",
  *           "answers": [
  *             {
- *               "answer_index": 0,
- *               "is_free_form": false,
- *               "content": "지원 과정"
- *             },
- *             {
- *               "answer_index": 5,
- *               "is_free_form": true,
- *               "content": "다른 의견"
+ *               options: [
+ *                 {
+ *                   "answer_index": 0,
+ *                   "is_free_form": false,
+ *                   "content": "지원 과정"
+ *                 },
+ *                 {
+ *                   "answer_index": 1,
+ *                   "is_free_form": true,
+ *                   "content": "다른 의견"
+ *                 }
+ *               ]
  *             }
  *           ]
  *          }
@@ -89,25 +93,24 @@ router.post('/answer', survey.saveAnswer);
  *           "question_index": 0,
  *           "question": "이 멘토를 통해서 얻고자 하는 것은?",
  *           "answers": [
- *           {
- *            options: [
  *             {
- *               "answer_index": 0,
- *               "is_free_form": false,
- *               "content": "지원 과정",
- *               "next_question_index": 1
- *             },
- *             {
- *               "answer_index": 5,
- *               "is_free_form": true,
- *               "content": "다른 의견",
- *               "next_question_index": 1
+ *               options: [
+ *                 {
+ *                   "answer_index": 0,
+ *                   "is_free_form": false,
+ *                   "content": "지원 과정",
+ *                   "next_question_index": 1
+ *                 },
+ *                 {
+ *                   "answer_index": 1,
+ *                   "is_free_form": true,
+ *                   "content": "다른 의견",
+ *                   "next_question_index": 1
+ *                 }
+ *               ]
  *             }
- *            ]
- *            }
- *            ]
  *           ]
- *          }
+ *         }
  *       ]
  *     }
  *
