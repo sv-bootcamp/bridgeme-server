d60d49161618468943e110dd26cdbd2526f5420e
SarahhhC <SarahhhC@users.noreply.github.com>
2016-10-28 18:02:32 +0900

    Sign in with general/optional information (#62)
    * edit general information
    * imageupload with s3 when profile pic update
    * Edit optional information (#63)

---
+++ src/server/config/json/jobcategory.js
@@ -0,0 +1,124 @@
+export default {
+  area: [
+    'Software Engineering',
+    'Design',
+    'Product Management',
+    'Data Science',
+    'Sales',
+    'Marketing',
+    'Recruiting / People Ops',
+    'Finance / Operations',
+    'Other',
+  ],
+  role: [
+    {
+      area: 'Software Engineering',
+      list: [
+        'Backend Engineer',
+        'Build/Release Engineer',
+        'Data Engineer',
+        'Database Administrator',
+        'DevOps Engineer',
+        'Embedded Engineer',
+        'Engineering Manager',
+        'Frontend Engineer',
+        'Full Stack Engineer',
+        'Machine Learning Engineer',
+        'Mobile Engineer',
+        'QA/Test Engineer',
+        'Security Engineer',
+        'UX Engineer',
+      ],
+    },
+    {
+      area: 'Design',
+      list: [
+        'Brand/Graphic Designer',
+        'Product Designer',
+        'UX Designer',
+        'UX Researcher',
+        'Visual/UI Designer',
+      ],
+    },
+    {
+      area: 'Product Management',
+      list: [
+        'Product Manager',
+      ],
+    },
+    {
+      area: 'Data Science',
+      list: [
+        'Data Analyst',
+        'Data Scientist',
+      ],
+    },
+    {
+      area: 'Sales',
+      list: [
+        'Account Executive',
+        'Account Manager',
+        'Business Development',
+        'Customer Success Manager',
+        'Sales Development Rep',
+        'Sales Engineer',
+        'Sales Manager',
+        'Sales Operations',
+      ],
+    },
+    {
+      area: 'Marketing',
+      list: [
+        'Acquisition Marketing',
+        'Brand Marketing',
+        'Community/Events Marketing',
+        'Content Marketing',
+        'Lifecycle/Email Marketing',
+        'Marketing Analytics & Ops',
+        'Marketing Programs',
+        'PR / Communications',
+        'Product Marketing',
+      ],
+    },
+    {
+      area: 'Recruiting / People Ops',
+      list: [
+        'HR Business Partner',
+        'People Ops Management',
+        'People Ops Specialist',
+        'Recruiter',
+        'Recruiting Coordinator',
+        'Sourcer',
+      ],
+    },
+    {
+      area: 'Finance / Operations',
+      list: [
+        'Accounting',
+        'Financial Management / Analysis',
+        'General Management',
+        'Office Administration',
+      ],
+    },
+    {
+      area: 'Other',
+      list: [
+        'Other',
+      ],
+    },
+  ],
+  years: [
+    '0-1 years',
+    '1-2 years',
+    '2-4 years',
+    '4-6 years',
+    '6+ years',
+  ],
+  education_background: [
+    "Associate's",
+    "Bachelor's",
+    "Master's",
+    'PhD',
+    'Other',
+  ],
+};
--- src/server/config/json/user.callback.js
+++ src/server/config/json/user.callback.js
@@ -4,7 +4,11 @@ export default {
   ERR_FAIL_AUTH: 'Authentication failed. Please sign in first.',
   ERR_INVALID_ACCESS_TOKEN: 'Invalid access_token.',
   ERR_INVALID_PLATFORM: 'Platforom not supported.',
+  ERR_INVALID_UPDATE: 'User Email should not be empty.',
   ERR_MONGOOSE: 'An error occured at mongoose.',
-  SUCCESS_SIGNIN: 'Sign in success.',
+  SUCCESS_EDIT: 'Edit success.',
   SUCCESS_REGISTER: 'Registration & Signin success.',
+  SUCCESS_SIGNIN: 'Sign in success.',
+  SUCCESS_UPDATE: 'Update success.',
+  SUCCESS_UPDATE_WITHOUT_IMAGE: 'Update success without image.',
 };
