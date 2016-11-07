56cba342b1099ccd513718c86298a14f4dd2e8b2
SarahhhC <joytutu29@naver.com>
2016-11-07 14:56:33 +0900

    add test form

--- src/test/controllers/match.controller.test.js
+++ src/test/controllers/match.controller.test.js
@@ -43,6 +43,10 @@ describe('Test Match API', function () {
         uri: API_BASE_URL + '/match/request',
         form: {
           mentor_id: 'invaliduserid',
+          message: {
+            subjects: "Geting a job",
+            contents: "hi",
+          },
         },
         jar: true,
         resolveWithFullResponse: true,
@@ -67,6 +71,10 @@ describe('Test Match API', function () {
         uri: API_BASE_URL + '/match/request',
         form: {
           mentor_id: userData.USER_A_DATA._id,
+          message: {
+            subjects: "Geting a job",
+            contents: "hi",
+          },
         },
         jar: true,
         resolveWithFullResponse: true,
