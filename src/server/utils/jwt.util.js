ca95117d0e0f4a57726071891026cf4e24ea6c01
Yangwoo Lee <lyw0149@gmail.com>
2016-11-14 14:43:24 +0900

    Modify jwtUtill.js

--- src/server/utils/jwt.util.js
+++ src/server/utils/jwt.util.js
@@ -3,18 +3,12 @@ import jwt from 'jsonwebtoken';
 
 const JWT_CREATE_OPTION = { algorithm: 'HS256', expiresIn: '60min' };
 
-let cert;
 const KEY_FILE = './jwt_key.pem';
-
-if (process.env.NODE_ENV === 'test') {
-  cert = 'thisistestsecret';
-} else {
-  cert = fs.readFileSync(KEY_FILE);
-}
+const KEY = process.env.NODE_ENV === 'test' ? 'thisistestsecret' : fs.readFileSync(KEY_FILE);
 
 export default {
   apiProtector(req, res, next) {
-    jwt.verify(req.headers.access_token, cert, function (err, decoded) {
+    jwt.verify(req.headers.access_token, KEY, function (err, decoded) {
       if (err) {
         res.status(401).json({ err_point: err.message });
       } else {
@@ -29,11 +23,11 @@ export default {
   },
 
   createAccessToken(user) {
-    return jwt.sign(this.generatePayload(user), cert, JWT_CREATE_OPTION);
+    return jwt.sign(this.generatePayload(user), KEY, JWT_CREATE_OPTION);
   },
 
   updateAccessToken(previousToken, updateTokenCallback) {
-    jwt.verify(previousToken, cert, { ignoreExpiration: true }, function (err, decodedUser) {
+    jwt.verify(previousToken, KEY, { ignoreExpiration: true }, function (err, decodedUser) {
       if (typeof updateTokenCallback === 'function') {
         updateTokenCallback(err, decodedUser ? this.createAccessToken(decodedUser) : undefined);
       }
