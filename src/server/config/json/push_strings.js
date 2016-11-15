86621ac6c036bf4333a9c073db7eda89adc3e7d9
KaylaKwon <papermoon703@gmail.com>
2016-11-15 17:24:08 +0900

    Create a push strings

---
+++ src/server/config/json/push_strings.js
@@ -0,0 +1,14 @@
+export default {
+  NEW_REQUEST: {
+    title: "New Request",
+    content: " has requested a connection to you.",
+  },
+  NEW_CONNECION: {
+    title: "New Connection",
+    content: " and you are connected now.",
+  },
+  NEW_MESSAGE: {
+    title: "New Message",
+    content: undefined,
+  },
+}
