35a475597de7bdf9824d2f9579f728c5199d6a79
SarahhhC <SarahhhC@users.noreply.github.com>
2016-12-12 17:25:42 +0900

    Mentorlist/bug fix (#97)
    * del console
    
    * edit pending
    
    * delete console

--- src/server/controllers/users.controller.js
+++ src/server/controllers/users.controller.js
@@ -60,11 +60,15 @@ export function getMentorList(req, res, next) {
         .sort({ stamp_login: -1 }).exec();
     })
     .then((user) => {
-      return Promise.all(user, user.map((item) => {
-        if (pendingList.includes(item._id.toString())) {
-          item.pending = true;
-        }
-      }));
+      const userData = JSON.parse(JSON.stringify(user));
+      return new Promise((resolve) => {
+        userData.forEach(item => {
+          if (pendingList.includes(item._id.toString())) {
+            item.pending = true;
+          }
+        });
+        resolve(userData);
+      });
     })
     .then((user) => {
       res.status(200).json(user);
