c93036afb2e2fed26e5bc3992bd267b3d68638c1
SarahhhC <joytutu29@naver.com>
2016-12-14 16:13:48 +0900

    merge

--- src/server/config/json/mail.strings.js
+++ src/server/config/json/mail.strings.js
@@ -3,8 +3,7 @@
  */
 
 export default {
-  REQUEST_HTML: '<h2>Hello there!</h2><h4>New mentee needs your mentoring.'
-    + 'How about visiting our app and seeing who is it?</h4>',
+  REQUEST_HTML: '<h2>Hello there!</h2><h4>New mentee needs your mentoring.',
   REQUEST_SUBJECT: 'New Mentoring Request!',
   YODA_ACCOUNT: '"BridgeMe Service Team" <yoda.mentor.lab@gmail.com>',
   RESETPW_HTML: '<h1>Hi,</br>The secret code to reset your password is following.</br></h1>',
