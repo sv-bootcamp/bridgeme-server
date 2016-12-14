54069bbe8bbd634d44387652694a3637cd4b823b
SarahhhC <joytutu29@naver.com>
2016-12-14 15:46:26 +0900

    add jwt key in db

--- src/server/utils/jwt.util.js
+++ src/server/utils/jwt.util.js
@@ -1,12 +1,17 @@
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
 const JWT_CREATE_OPTION = { algorithm: 'HS256', expiresIn: '60min' };
+const Key = mongoose.model('key');
 
-const KEY = process.env.NODE_ENV === 'test' ? 'thisistestsecret' : 'thisisproductsecret';
+const jwt_key = new Promise((resolve) => {
+  return Key.findOne({ name: 'jwtKey' }).exec()
+    .then(jwtObject => resolve(jwtObject.key));
+}).toString();
 
 export default {
   apiProtector(req, res, next) {
-    jwt.verify(req.headers.access_token, KEY, function (err, decoded) {
+    jwt.verify(req.headers.access_token, jwt_key, function (err, decoded) {
       if (err) {
         res.status(401).json({ err_point: err.message });
       } else {
@@ -21,11 +26,11 @@ export default {
   },
 
   createAccessToken(user) {
-    return jwt.sign(this.generatePayload(user), KEY, JWT_CREATE_OPTION);
+    return jwt.sign(this.generatePayload(user), jwt_key, JWT_CREATE_OPTION);
   },
 
   updateAccessToken(previousToken, updateTokenCallback) {
-    jwt.verify(previousToken, KEY, { ignoreExpiration: true }, function (err, decodedUser) {
+    jwt.verify(previousToken, jwt_key, { ignoreExpiration: true }, function (err, decodedUser) {
       if (typeof updateTokenCallback === 'function') {
         updateTokenCallback(err, decodedUser ? this.createAccessToken(decodedUser) : undefined);
       }
