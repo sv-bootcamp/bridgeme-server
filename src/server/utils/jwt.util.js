212e06846c4e9993b34de91d1ac1ac766b649d19
SarahhhC <joytutu29@naver.com>
2016-12-14 17:49:40 +0900

    edit jwt key

--- src/server/utils/jwt.util.js
+++ src/server/utils/jwt.util.js
@@ -4,14 +4,17 @@ import mongoose from 'mongoose';
 const JWT_CREATE_OPTION = { algorithm: 'HS256', expiresIn: '60min' };
 const Key = mongoose.model('key');
 
-const jwt_key = new Promise((resolve) => {
-  return Key.findOne({ name: 'jwtKey' }).exec()
-    .then(jwtObject => resolve(jwtObject.key));
-}).toString();
+const jwt_key = Key.findOne({ name: 'jwtKey' }).exec()
+  .then((jwtObject) => {
+    return jwtObject.key;
+  })
+  .catch((err) => {
+
+  });
 
 export default {
   apiProtector(req, res, next) {
-    jwt.verify(req.headers.access_token, jwt_key, function (err, decoded) {
+    jwt.verify(req.headers.access_token, jwt_key.emitted.fulfill[0], function (err, decoded) {
       if (err) {
         res.status(401).json({ err_point: err.message });
       } else {
@@ -26,11 +29,12 @@ export default {
   },
 
   createAccessToken(user) {
-    return jwt.sign(this.generatePayload(user), jwt_key, JWT_CREATE_OPTION);
+    console.log(jwt_key.emitted.fulfill[0]);
+    return jwt.sign(this.generatePayload(user), jwt_key.emitted.fulfill[0], JWT_CREATE_OPTION);
   },
 
   updateAccessToken(previousToken, updateTokenCallback) {
-    jwt.verify(previousToken, jwt_key, { ignoreExpiration: true }, function (err, decodedUser) {
+    jwt.verify(previousToken, jwt_key.emitted.fulfill[0], { ignoreExpiration: true }, function (err, decodedUser) {
       if (typeof updateTokenCallback === 'function') {
         updateTokenCallback(err, decodedUser ? this.createAccessToken(decodedUser) : undefined);
       }
