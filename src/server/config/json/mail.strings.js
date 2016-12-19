3bc053acbd092ad86a58e808b7184fa67b373851
SarahhhC <SarahhhC@users.noreply.github.com>
2016-12-19 15:22:55 +0900

    Request Mail fixed (#100)
    * initial setting
    
    * error handler looks like this
    
    * edit pending
    
    * merge
    
    * add contents
    
    * edit contents
    
    * feedback from designer
    
    * adjustment comments

--- src/server/config/json/mail.strings.js
+++ src/server/config/json/mail.strings.js
@@ -3,9 +3,11 @@
  */
 
 export default {
-  REQUEST_HTML: '<h1>Hi!</br>New mentee needs your mentoring.</h1>',
-  REQUEST_SUBJECT: 'New mentee needs your help!',
-  YODA_ACCOUNT: '"Yod Service Team" <yoda.mentor.lab@gmail.com>',
+  REQUEST_TITLE: '<h3>Hello there!</h3></br></br><h4>',
+  REQUEST_BODY: ' has requested a connection to you!</h4>',
+  REQUEST_CONTENTS: '<h4>Please check this request on BridgeMe app.</h4>',
+  REQUEST_SUBJECT: 'You’ve got a new connection request.',
+  YODA_ACCOUNT: '"BridgeMe Service Team" <yoda.mentor.lab@gmail.com>',
   RESETPW_HTML: '<h1>Hi,</br>The secret code to reset your password is following.</br></h1>',
   RESETPW_SUBJECT: 'Reset Your Password of BridgeMe.',
 };
