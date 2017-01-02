b8f5a1b9ef0d16810c7de6d7c11b04c3f0e4ff7d
KaylaKwon <papermoon703@gmail.com>
2017-01-02 14:07:19 +0900

    Updated expertise data. Inserted index property for enum comparison

--- src/server/config/json/expertise.data.js
+++ src/server/config/json/expertise.data.js
@@ -1,9 +1,33 @@
+/*
+ Data for expertise filter.
+ */
 export const expertiseData = [
-  'Study abroad',
-  'Career advice',
-  'Portfolio & Resume',
-  'Startup',
-  'Career change',
-  'Networking',
-  'Soft skills',
+  {
+    index: 1,
+    content: 'Study abroad',
+  },
+  {
+    index: 2,
+    content: 'Career advice',
+  },
+  {
+    index: 3,
+    content: 'Portfolio & Resume',
+  },
+  {
+    index: 4,
+    content: 'Startup',
+  },
+  {
+    index: 5,
+    content: 'Career change',
+  },
+  {
+    index: 6,
+    content: 'Networking',
+  },
+  {
+    index: 7,
+    content: 'Soft skills',
+  },
 ];
