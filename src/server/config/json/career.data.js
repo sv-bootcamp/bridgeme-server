560793d3809f999673b8079dddca0c35af20f11e
KaylaKwon <papermoon703@gmail.com>
2017-01-02 13:49:50 +0900

    Updated career data. Inserted index for enum comparison.

--- src/server/config/json/career.data.js
+++ src/server/config/json/career.data.js
@@ -1,124 +1,349 @@
 export const CareerData = {
-  area: [
-    'Software Engineering',
-    'Design',
-    'Product Management',
-    'Data Science',
-    'Sales',
-    'Marketing',
-    'Recruiting / People Ops',
-    'Finance / Operations',
-    'Other',
-  ],
   role: [
     {
+      index: 0,
       area: 'Software Engineering',
       list: [
-        'Backend Engineer',
-        'Build/Release Engineer',
-        'Data Engineer',
-        'Database Administrator',
-        'DevOps Engineer',
-        'Embedded Engineer',
-        'Engineering Manager',
-        'Frontend Engineer',
-        'Full Stack Engineer',
-        'Machine Learning Engineer',
-        'Mobile Engineer',
-        'QA/Test Engineer',
-        'Security Engineer',
-        'UX Engineer',
+        {
+          index: 0,
+          content: 'Backend Engineer',
+        },
+        {
+          index: 1,
+          content: 'Build / Release Engineer',
+        },
+        {
+          index: 2,
+          content: 'Data Engineer',
+        },
+        {
+          index: 3,
+          content: 'Database Administrator',
+        },
+        {
+          index: 4,
+          content: 'DevOps Engineer',
+        },
+        {
+          index: 5,
+          content: 'Embedded Engineer',
+        },
+        {
+          index: 6,
+          content: 'Engineering Manager',
+        },
+        {
+          index: 7,
+          content: 'Frontend Engineer',
+        },
+        {
+          index: 8,
+          content: 'Full Stack Engineer',
+        },
+        {
+          index: 9,
+          content: 'Machine Learning Engineer',
+        },
+        {
+          index: 10,
+          content: 'Mobile Engineer',
+        },
+        {
+          index: 11,
+          content: 'QA / Test Engineer',
+        },
+        {
+          index: 12,
+          content: 'Security Engineer',
+        },
+        {
+          index: 13,
+          content: 'UX Engineer',
+        },
       ],
     },
     {
+      index: 1,
       area: 'Design',
       list: [
-        'Brand/Graphic Designer',
-        'Product Designer',
-        'UX Designer',
-        'UX Researcher',
-        'Visual/UI Designer',
+        {
+          index: 0,
+          content: 'Brand / Graphic Designer',
+        },
+        {
+          index: 1,
+          content: 'Product Designer',
+        },
+        {
+          index: 2,
+          content: 'UX Designer',
+        },
+        {
+          index: 3,
+          content: 'UX Researcher',
+        },
+        {
+          index: 4,
+          content: 'Visual / UI Designer',
+        },
       ],
     },
     {
+      index: 2,
       area: 'Product Management',
       list: [
-        'Product Manager',
+        {
+          index: 0,
+          content: 'Product Manager',
+        },
       ],
     },
     {
+      index: 3,
       area: 'Data Science',
       list: [
-        'Data Analyst',
-        'Data Scientist',
+        {
+          index: 0,
+          content: 'Data Analyst',
+        },
+        {
+          index: 1,
+          content: 'Data Scientist',
+        },
+
       ],
     },
     {
+      index: 4,
       area: 'Sales',
       list: [
-        'Account Executive',
-        'Account Manager',
-        'Business Development',
-        'Customer Success Manager',
-        'Sales Development Rep',
-        'Sales Engineer',
-        'Sales Manager',
-        'Sales Operations',
+        {
+          index: 0,
+          content: 'Account Executive',
+        },
+        {
+          index: 1,
+          content: 'Account Manager',
+        },
+        {
+          index: 2,
+          content: 'Business Development',
+        },
+        {
+          index: 3,
+          content: 'Customer Success Manager',
+        },
+        {
+          index: 4,
+          content: 'Sales Development Rep',
+        },
+        {
+          index: 5,
+          content: 'Sales Engineer',
+        },
+        {
+          index: 6,
+          content: 'Sales Manager',
+        },
+        {
+          index: 7,
+          content: 'Sales Operations',
+        },
       ],
     },
     {
+      index: 5,
       area: 'Marketing',
       list: [
-        'Acquisition Marketing',
-        'Brand Marketing',
-        'Community/Events Marketing',
-        'Content Marketing',
-        'Lifecycle/Email Marketing',
-        'Marketing Analytics & Ops',
-        'Marketing Programs',
-        'PR / Communications',
-        'Product Marketing',
+        {
+          index: 0,
+          content: 'Acquisition Marketing',
+        },
+        {
+          index: 1,
+          content: 'Brand Marketing',
+        },
+        {
+          index: 2,
+          content: 'Community / Events Marketing',
+        },
+        {
+          index: 3,
+          content: 'Content Marketing',
+        },
+        {
+          index: 4,
+          content: 'Lifecycle / Email Marketing',
+        },
+        {
+          index: 5,
+          content: 'Marketing Analytics & Ops',
+        },
+        {
+          index: 6,
+          content: 'Marketing Programs',
+        },
+        {
+          index: 7,
+          content: 'PR / Communications',
+        },
+        {
+          index: 8,
+          content: 'Product Marketing',
+        },
       ],
     },
     {
+      index: 6,
       area: 'Recruiting / People Ops',
       list: [
-        'HR Business Partner',
-        'People Ops Management',
-        'People Ops Specialist',
-        'Recruiter',
-        'Recruiting Coordinator',
-        'Sourcer',
+        {
+          index: 0,
+          content: 'HR Business Partner',
+        },
+        {
+          index: 1,
+          content: 'People Ops Management',
+        },
+        {
+          index: 2,
+          content: 'People Ops Specialist',
+        },
+        {
+          index: 3,
+          content: 'Recruiter',
+        },
+        {
+          index: 4,
+          content: 'Recruiting Coordinator',
+        },
+        {
+          index: 5,
+          content: 'Sourcer',
+        },
       ],
     },
     {
+      index: 7,
       area: 'Finance / Operations',
       list: [
-        'Accounting',
-        'Financial Management / Analysis',
-        'General Management',
-        'Office Administration',
+        {
+          index: 0,
+          content: 'Accounting',
+        },
+        {
+          index: 1,
+          content: 'Financial Management / Analysis',
+        }
+        ,
+        {
+          index: 2,
+          content: 'General Management',
+        }
+        ,
+        {
+          index: 3,
+          content: 'Office Administration',
+        },
       ],
     },
     {
+      index: 8,
       area: 'Other',
       list: [
-        'Other',
+        {
+          index: 0,
+          content: 'Other',
+        },
       ],
     },
   ],
   years: [
-    '0-1 years',
-    '1-2 years',
-    '2-4 years',
-    '4-6 years',
-    '6+ years',
+    {
+      index: 0,
+      content: '0-1 years',
+    },
+    {
+      index: 1,
+      content: '1-2 years',
+    }
+    ,
+    {
+      index: 2,
+      content: '2-4 years',
+    }
+    ,
+    {
+      index: 3,
+      content: '4-6 years',
+    }
+    ,
+    {
+      index: 4,
+      content: '6+ years',
+    },
+  ],
+  area: [
+    {
+      index: 0,
+      content: 'Software Engineering',
+    },
+    {
+      index: 1,
+      content: 'Design',
+    },
+    {
+      index: 2,
+      content: 'Product Management',
+    },
+    {
+      index: 3,
+      content: 'Data Science',
+    },
+    {
+      index: 4,
+      content: 'Sales',
+    },
+    {
+      index: 5,
+      content: 'Marketing',
+    },
+    {
+      index: 6,
+      content: 'Recruiting / People Ops',
+    },
+    {
+      index: 7,
+      content: 'Finance / Operations',
+    },
+    {
+      index: 8,
+      content: 'Other',
+    },
   ],
   education_background: [
-    "Associate's",
-    "Bachelor's",
-    "Master's",
-    'PhD',
-    'Other',
+    {
+      index: 0,
+      content: "Associate's",
+    },
+    {
+      index: 1,
+      content: "Bachelor's",
+    }
+    ,
+    {
+      index: 2,
+      content: "Master's",
+    }
+    ,
+    {
+      index: 3,
+      content: 'PhD',
+    }
+    ,
+    {
+      index: 4,
+      content: 'Other',
+    },
   ],
 };
